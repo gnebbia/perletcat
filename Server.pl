a5989804054268b7028ba503d5fb63f31f4e45ab
gnebbia <nebbia@openmailbox.org>
2017-03-29 10:16:07 +0200

    Initial commit

---
+++ Server.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use IO::Socket::INET;
+ 
+# auto-flush on socket
+$| = 1;
+ 
+# creating a listening socket
+my $socket = new IO::Socket::INET (
+    LocalHost => '0.0.0.0',
+    LocalPort => '7777',
+    Proto => 'tcp',
+    Listen => 5,
+    Reuse => 1
+);
+die "cannot create socket $!\n" unless $socket;
+print "server waiting for client connection on port 7777\n";
+ 
+# waiting for a new client connection
+my $client_socket = $socket->accept();
+
+# get information about a newly connected client
+my $client_address = $client_socket->peerhost();
+my $client_port = $client_socket->peerport();
+print "connection from $client_address:$client_port\n";
+
+while(1)
+{
+ 
+    # read up to 1024 characters from the connected client
+    my $data = "";
+    $client_socket->recv($data, 1024);
+    print "received data: $data\n";
+ 
+    # write response data to the connected client
+    $data = "ok\n";
+    $client_socket->send($data);
+ 
+    # notify client that response has been sent
+#    shutdown($client_socket, 1);
+}
+ 
+$socket->close();
